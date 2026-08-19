aacc603161f44df23aa515a4a451f6c71ca16eee
Aline Vernier <aline.vernier@ensta.fr>
2026-08-19 11:52:42 +0200

    Started very small loop manager

---
+++ main.py
@@ -0,0 +1,36 @@
+# libraries
+import sys
+import logging
+
+from PyQt6.QtWidgets import QApplication
+from laplace_log import LoggerLHC, log
+from laplace_server.protocol import LOGGER_NAME
+
+# Initialize the logger
+LoggerLHC("laplace.opt", file_level="debug", console_level="info")
+log.info("Starting loopWindow...")
+
+logging.getLogger(LOGGER_NAME).setLevel(logging.INFO)
+logging.getLogger("matplotlib").setLevel(logging.WARNING)
+
+from laplace_log import uncaught_exception
+
+
+import os 
+print(os.getcwd())
+# project
+from .interface import LoopWindow
+
+if __name__ == "__main__":
+
+    print(os.getcwd())
+    app = QApplication(sys.argv) # create the app
+    window = LoopWindow()         # create the window
+    window.show()                # display the window
+    
+    log.info("Window opened.")
+
+    # end the process
+    exit_code = app.exec()
+    log.info(f"Application is exiting with code {exit_code}.")
+    sys.exit(exit_code)
